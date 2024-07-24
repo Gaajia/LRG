65ef729e6e6d29c92edbe8ab20bc9a7766d22d93
rogers7levi <rogers.levi714@gmail.com>
2024-07-24 15:37:51 +0100

    Grid creation complete

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const canvas = document.getElementById('ourCanvas')
+const ctx = canvas.getContext('2d')
 
+const gridSize = canvas.width / 6;
+const gridWidth = 6;
+const gridHeight = 6;
+
+function drawGrid() {
+    for(let i = 0; i <= gridWidth; i++){
+        ctx.moveTo(i * gridSize, 0);
+        ctx.lineTo(i * gridSize, canvas.height);
+    }
+    for(let j = 0; j<=gridHeight; j++){
+        ctx.moveTo(0, j * gridSize);
+        ctx.lineTo(canvas.width, j * gridSize);
+    }
+
+    ctx.strokeStyle = 'black';
+    ctx.stroke()
+}
+
+
+
+drawGrid()
