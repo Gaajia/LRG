1660923633d29b7c5d43cdd6572a1f6f2a6013d2
rogers7levi <rogers.levi714@gmail.com>
2024-07-24 16:58:15 +0100

    snakes food

--- script.js
+++ script.js
@@ -20,6 +20,43 @@ function drawGrid() {
     ctx.strokeStyle = 'black';
     ctx.stroke()
 }
+const canvas = document.getElementById('ourCanvas')
+const ctx = canvas.getContext('2d')
+const gridSize = canvas.width / 6;
+const gridWidth = 6;
+const gridHeight = 6;
+let appleX, appleY;
+
+
+
+function drawGrid() {
+    for(let i = 0; i <= gridWidth; i++){
+        ctx.moveTo(i * gridSize, 0);
+        ctx.lineTo(i * gridSize, canvas.height);
+    }
+    for(let j = 0; j<=gridHeight; j++){
+        ctx.moveTo(0, j * gridSize);
+        ctx.lineTo(canvas.width, j * gridSize);
+    }
+
+    ctx.strokeStyle = 'black';
+    ctx.stroke()
+}
+
+function generateApple(){
+    appleX = Math.floor(Math.random() * gridWidth) * gridSize;
+    appleY = Math.floor(Math.random() * gridHeight) * gridSize;
+}
+
+function drawApple(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect(appleX,appleY,gridSize,gridSize)
+}
+
+
+drawGrid();
+generateApple();
+drawApple();
 
 drawGrid();
 
