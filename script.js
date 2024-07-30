1f9dd05fe89a5b7b5c3bd2b8c05ac780f8de5aa9
rilez366 <rilez366@outlook.com>
2024-07-30 12:17:33 +0100

    High score (#27)
    * styles for current and high score.
    
    * mark up for current and high scores
    
    * Current score and high score set to 0
    
    * created a utils folder in which functions can be imported and exported.

--- script.js
+++ script.js
@@ -1,63 +1,65 @@
-const canvas = document.getElementById('ourCanvas')
-const ctx = canvas.getContext('2d')
-const gridWidth = 20;
-const gridHeight = 20;
-const gridSize = canvas.width / 20;
+import { setHighScore, setCurrentScore, highScore, currentScore } from "./utils/score-values.js";
+
+const canvas = document.getElementById('ourCanvas');
+const ctx = canvas.getContext('2d');
+const gridSize = canvas.width / 6;
+const gridWidth = 6;
+const gridHeight = 6;
 let appleX, appleY;
 let simulatedSnakeX = 5 * gridSize;
 let simulatedSnakeY = 5 * gridSize;
 
 function drawGrid() {
-    for(let i = 0; i <= gridWidth; i++){
+    for (let i = 0; i <= gridWidth; i++) {
         ctx.moveTo(i * gridSize, 0);
         ctx.lineTo(i * gridSize, canvas.height);
     }
-    for(let j = 0; j<=gridHeight; j++){
+    for (let j = 0; j <= gridHeight; j++) {
         ctx.moveTo(0, j * gridSize);
         ctx.lineTo(canvas.width, j * gridSize);
     }
 
     ctx.strokeStyle = 'black';
-    ctx.stroke()
+    ctx.stroke();
 }
 
-function generateApple(){
+function generateApple() {
     appleX = Math.floor(Math.random() * gridWidth) * gridSize;
     appleY = Math.floor(Math.random() * gridHeight) * gridSize;
 }
 
-function drawApple(){
+function drawApple() {
     ctx.fillStyle = 'red';
-    ctx.fillRect(appleX,appleY,gridSize,gridSize)
+    ctx.fillRect(appleX, appleY, gridSize, gridSize);
 }
 
-function createCollision(x,y){
-    if(x < 0 || x >= canvas.width || y < 0 || y >= canvas.height){
+function createCollision(x, y) {
+    if (x < 0 || x >= canvas.width || y < 0 || y >= canvas.height) {
         alert(`You've hit the wall! Try again!`);
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 
-function gameLoop(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawGrid();
     drawApple();
     // this will be for the snake
-    const simulatedSnakeX = 2 * gridSize
-    const simulatedSnakeY = 2 * gridSize
-    createCollision(simulatedSnakeX,simulatedSnakeY)
+    const simulatedSnakeX = 2 * gridSize;
+    const simulatedSnakeY = 2 * gridSize;
+    createCollision(simulatedSnakeX, simulatedSnakeY);
 }
 
-
-
-if(createCollision(simulatedSnakeX,simulatedSnakeY)){
-    console.log('Collision detected at:', simulatedSnakeX, simulatedSnakeY)
+if (createCollision(simulatedSnakeX, simulatedSnakeY)) {
+    console.log('Collision detected at:', simulatedSnakeX, simulatedSnakeY);
 } else {
-    console.log("No collision detected", simulatedSnakeX, simulatedSnakeY)
+    console.log("No collision detected", simulatedSnakeX, simulatedSnakeY);
 }
 
 generateApple();
-setInterval(gameLoop,200)
+setInterval(gameLoop, 200);
 
+setHighScore();
+setCurrentScore();
 
